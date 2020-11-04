123602523c0957c141fe9755fb71b8401b1a3be8
Penguin98kStudio <36449676+Penguin98kStudio@users.noreply.github.com>
2020-11-04 21:07:34 +0500

    Update JsonYapfFormat.py

--- JsonYapfFormat.py
+++ JsonYapfFormat.py
@@ -2,6 +2,8 @@ import sublime
 import sublime_plugin
 
 import subprocess
+import re
+import textwrap
 
 
 class JsonYapfFormatCommand(sublime_plugin.TextCommand):
@@ -10,18 +12,37 @@ class JsonYapfFormatCommand(sublime_plugin.TextCommand):
 		content = self.view.substr(sublime.Region(0, self.view.size()))
 		content = content.replace("//", "#")
 		content = content.replace("res:#", "res://")
+		indexes = re.finditer(",\\s*[\\)\\]\\}]", content)
+		indexes = [i.start() for i in indexes]
+		content = "".join(
+			[char for i, char in enumerate(content) if i not in indexes]
+		)
+		content = content.replace("true","True").replace("false","False")
+		if '\\' not in sublime.packages_path():
+			path = sublime.packages_path() + '/JSONFORMATTERF/'
+		else:
+			path = sublime.packages_path() + '\\JSONFORMATTERF\\'
 
-		path = sublime.packages_path() + '\\JsonFormatter\\'
 		with open(path + 'temporary.notpy', 'w') as file:
 			file.write(content)
+
 		command = 'yapf "' + path + 'temporary.notpy" -i --style style.txt'
+		print("Running"+command)
 		process = subprocess.Popen(command, shell=True)
-		process.wait()
+		error = ""
+		if process.wait() != 0:
+			error = (
+				"Error: Yapf module is either not installed or not on path or Something Went Wrong while YAPF was trying to Format the File"
+			)
 		with open(path + 'temporary.notpy', 'r') as file:
 			content = file.read()
 		content = content.replace("#", "//")
+		content = content.replace("True","true").replace("False","false")
+		if error:
+			print(error)
+			sublime.set_timeout_async(lambda: sublime.status_message(error), 1000)
+	
 		self.view.replace(edit, sublime.Region(0, self.view.size()), content)
-		sublime.status_message("Formatted File")
 
 	def is_enabled(self):
 		return self.view.match_selector(0, "source.json")
